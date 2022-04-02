5ec0203a1a3e9f3384c627d3d8c0536b347518a8
HendrikCromboom <hcromboom@gmail.com>
2022-04-02 14:10:26 +0200

    NUnit -> MSTest
    Moved from NUnit to MSTest and refactored the tests since the ddl's seemed to act up. Seems like a dependency I can't be bothered with right now. Used to work a-ok in the past...

---
+++ MazeTest/AmazingTests.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MazeTest
+{
+    [TestClass]
+    public class AmazingTests2
+    {
+        [TestMethod]
+        public void Seed0size15x20()
+        {
+            String expected = @"Amazing - Copyright by Creative Computing, Morristown, NJ
+:--:--:--:--:--:--:--:--:--:--:  :--:--:--:--:
+I                             I        I     I 
+:  :--:--:--:--:  :--:--:  :--:  :--:--:  :--:
+I           I     I     I  I     I     I     I 
+:--:--:--:  :  :  :  :  :--:  :  :  :  :--:  :
+I        I  I  I     I  I     I     I     I  I 
+:--:  :  :--:  :  :--:  :  :--:--:--:--:  :  :
+I     I  I     I     I     I  I           I  I 
+:  :--:  :  :--:--:  :--:--:  :  :--:  :--:  :
+I  I  I     I        I     I  I     I     I  I 
+:  :  :  :--:  :--:--:  :--:  :--:  :--:  :  :
+I  I  I     I  I     I        I     I  I  I  I 
+:  :  :--:  :--:  :  :  :--:--:  :--:  :  :  :
+I  I     I     I  I  I        I  I     I  I  I 
+:  :  :  :--:  :--:  :--:  :  :  :  :--:  :  :
+I  I  I     I     I  I     I  I  I        I  I 
+:  :--:--:  :  :  :  :  :  :--:  :  :--:--:  :
+I           I  I     I  I  I     I     I  I  I 
+:--:--:--:--:  :--:--:  :  :  :--:--:  :  :  :
+I     I  I        I     I  I  I        I     I 
+:  :  :  :  :  :  :  :--:  :  :--:--:--:  :--:
+I  I  I     I  I     I     I  I        I     I 
+:--:  :  :--:  :--:--:  :--:  :  :--:  :--:  :
+I     I  I     I        I     I  I     I  I  I 
+:  :--:  :  :--:  :--:--:  :--:  :  :--:  :  :
+I        I  I     I     I     I  I        I  I 
+:  :--:--:  :--:--:  :  :--:  :  :--:--:  :  :
+I        I     I     I     I  I  I     I     I 
+:--:--:  :--:  :  :  :--:  :  :  :  :--:--:  :
+I     I     I  I  I  I     I     I  I        I 
+:  :  :--:  :  :  :  :  :--:--:--:  :  :--:--:
+I  I        I     I  I           I  I  I     I 
+:  :--:--:--:--:--:--:--:  :--:  :  :  :--:  :
+I  I        I  I  I     I     I  I           I 
+:  :--:  :  :--:--:  :  :--:  :--:--:--:--:--:
+I     I  I  I     I  I  I           I        I 
+:--:  :  :  :  :  :  :--:  :--:--:--:  :--:--:
+I     I  I     I     I              I        I 
+:  :--:--:--:--:  :  :  :  :  :--:  :--:--:  :
+I                 I  I     I     I           I 
+:--:--:--:--:--:--:--:--:--:--:--:  :--:--:--:
+";
+
+            Amazing.random = new Random(0);
+            Amazing.doit(15, 20);
+
+            Assert.AreEqual(expected, Amazing.result, "Should have the maze that was expected");
+        }
+
+        [TestMethod]
+        public void Seed100size4x5()
+        {
+            String expected = @"Amazing - Copyright by Creative Computing, Morristown, NJ
+:--:--:--:  :
+I           I 
+:  :--:  :--:
+I     I  I  I 
+:--:  :  :  :
+I  I  I     I 
+:  :  :--:  :
+I  I     I  I 
+:  :--:  :  :
+I        I  I 
+:--:--:--:  :
+";
+
+            Amazing.random = new Random(100);
+            Amazing.doit(4, 5);
+
+            Assert.AreEqual(expected, Amazing.result, "Should have the maze that was expected");
+        }
+    }
+}
